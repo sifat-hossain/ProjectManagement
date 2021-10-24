ff592197939dd576b139276a8e10db7607a370aa
Sifat <Sifat@DESKTOP-KV0F3SL>
2021-10-24 16:14:23 +0600

    Create and View of tender opening and also create controller

--- ViewModel/TenderOpeningViewModel.cs
+++ ViewModel/TenderOpeningViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using ProjectManagement.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
@@ -11,7 +12,9 @@ namespace ProjectManagement.ViewModel
     {
         public int TenderOpeningId { get; set; }
         public int? ProjectId { get; set; }
+        [DataType(DataType.Date)]
         public DateTime? TenderOpeningDate { get; set; }
+        [DataType(DataType.Date)]
         public DateTime? TenderClosingDate { get; set; }
         public decimal? TenderHonorium { get; set; }
         public string ProjectName { get; set; }
