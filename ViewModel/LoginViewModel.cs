8c1269a763870caacb6bf827a10a870475c66644
itojahid <ito.jahid@gmail.com>
2022-02-15 10:04:33 +0600

    Login UI, Auth Connection, SweetAlert added

---
+++ ViewModel/LoginViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectManagement.ViewModel
+{
+    public class LoginViewModel
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Token { get; set; }
+        public Boolean RememberMe { get; set; }
+
+    }
+}
